113011f0324815b8b55ddea71923266c19e6da58
adriarodrisan <1900adria@gmail.com>
2023-12-12 14:23:05 +0100

    he canviado los documentos

--- hola.py
+++ hola.py
@@ -9,9 +9,8 @@ longitud_dones = {
     "taronja": 590
 }
 n = len(array)
-for i in range(n-1):
-    for j in range(0,i+1, -1):
-        if longitud_dones(array[j]) < longitud_dones(array[min_index]):
-            min_index = j
-            array[i], array[min_index] = array[min_index], array[i]
-    print(array)
+for i in range(n - 1):
+    for j in range(0,n - i - 1):
+        if longitud_dones[array[j]] < longitud_dones[array[j+1]]:
+            array[i], array[j + 1] = array[j + 1], array[i]
+print(array)
--- pruebas.py
+++ pruebas.py
@@ -12,6 +12,5 @@ n = len(array)
 for i in range(n - 1):
     for j in range(0, n - i - 1):
         if longitud_dones[array[j]] > longitud_dones[array[j + 1]]:
-            # Intercanviem els elements si estan fora d'ordre
             array[j], array[j + 1] = array[j + 1], array[j]
 print(array)
