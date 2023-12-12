a19cd807129a72ba8044cb68d2153946bd5d7af5
adriarodrisan <1900adria@gmail.com>
2023-12-12 14:30:05 +0100

    he canviado los documentos

--- pruebas.py
+++ pruebas.py
@@ -8,9 +8,8 @@ longitud_dones = {
     "cian": 500,
     "taronja": 590
 }
-n = len(array)
-for i in range(n - 1):
-    for j in range(0, n - i - 1):
+for i in range(0, len(array)):
+    for j in range(0, ):
         if longitud_dones[array[j]] > longitud_dones[array[j + 1]]:
             array[j], array[j + 1] = array[j + 1], array[j]
 print(array)
