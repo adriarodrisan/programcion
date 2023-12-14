480191ae409a0300b6fbe0c34fbc73bbf2ad4ce2
adriarodrisan <1900adria@gmail.com>
2023-12-14 10:13:56 +0100

    canviar nombre i arreglar errores en el codigo

--- coloresbubblesort.py
+++ coloresbubblesort.py
@@ -9,8 +9,8 @@ longitud_dones = {
     "taronja": 590
 }
 n = len(array)
-for i in range(n - 1):
-    for j in range(0,n - i - 1):
-        if longitud_dones[array[j]] < longitud_dones[array[j+1]]:
-            array[i], array[j + 1] = array[j+1], array[i]
+for i in range(0, len(array)):
+    for j in range(0, n-i-1 ):
+        if longitud_dones[array[j]] > longitud_dones[array[j + 1]]:
+            array[j], array[j + 1] = array[j + 1], array[j]
 print(array)
