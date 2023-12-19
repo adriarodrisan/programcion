5185cba98df408dc75bc0f4d38136f6c9d28ad4c
adriarodrisan <1900adria@gmail.com>
2023-12-19 13:22:04 +0100

    arreglando errores en el codigo

--- practicagithub.py
+++ practicagithub.py
@@ -1,8 +1,8 @@
 #PRE:introdueix un llista de tuples amb la informació requerida
 estudiante1=[]
 estudiante2=[]
-llista1={}
-llista2={}
+llista1=[]
+llista2=[]
 #Afegir elements a la llista d'estudisantes(Nom, Cognom, Edat, Nota1, Nota2, Nota3).
 nombre1=str(input("introduce el nombre del alumno: " ))
 apellido=str(input("Introduzca los apellidos del estudiante: ") )
@@ -10,30 +10,30 @@ edad=int(input("Introduzca la edad del estudiante: "))
 nota1_1= float(input("Introduzca la nota del primer examen del estudiante: "))
 nota2_1= float(input("Introduzca la nota del segundo examen del estudiante: "))
 nota3_1= float(input("Introduzca la nota del tercer examen del estudiante: "))
-tupla=(nombre1,apellido,edad,nota1_1,nota2_1,nota3_1)
-estudiante1.append(tupla)
 mediaponderada1=nota1_1*0.3+nota2_1*0.4+nota3_1*0.3
+tupla1=(nombre1,apellido,edad,nota1_1,nota2_1,nota3_1, mediaponderada1)
+estudiante1.append(tupla1)
 nombre2=str(input("introduce el nombre del alumno: " ))
 apellido=str(input("Introduzca los apellidos del estudiante: ") )
 edad=int(input("Introduzca la edad del estudiante: "))
 nota1_2= float(input("Introduzca la nota del primer examen del estudiante: "))
 nota2_2= float(input("Introduzca la nota del segundo examen del estudiante: "))
 nota3_2= float(input("Introduzca la nota del tercer examen del estudiante: "))
-tupla=(nombre2,apellido,edad,nota1_2,nota2_2,nota3_2)
-estudiante2.append(tupla)
 mediaponderada2=nota1_2*0.3+nota2_2*0.4+nota3_2*0.3
+tupla2=(nombre2,apellido,edad,nota1_2,nota2_2,nota3_2, mediaponderada2)
+estudiante2.append(tupla2)
+
 if mediaponderada1 >=7:
-    llista1[nombre1]=mediaponderada1
+    llista1.append(tupla1)
 else:
     if mediaponderada2 >=7:
-        llista1[nombre2]=mediaponderada2
-        
+        llista1.append(tupla2)
 if nota1_1>=8 or nota2_1>=8 or nota3_1>=8:
-    llista2[nombre1]=mediaponderada1
+   llista2.append(tupla1)
     
 else:
     if nota1_2>=8 or nota2_2>=8 or nota3_2>=8:
-        llista2[nombre2]=mediaponderada2
+        llista2.append(tupla2)
 print(llista1)
 print(llista2) 
 #POST: devuelve el nombre i la media de sus examenes
