ac02a49767fcbf69b222ef6ca5b47a777bf22a15
adriarodrisan <1900adria@gmail.com>
2023-12-19 12:54:03 +0100

    añadimos dos for para contar las palabras

--- estructures de repetició.py
+++ estructures de repetició.py
@@ -1,4 +1,7 @@
 #PRE:lleguir el text i comti la seva freqüencia
-texto= str(input("Dime un texto que quieras aqui pero no te pases"))
+texto= str(input("Dime un texto que quieras aqui pero no te pases: "))
 texto= texto.casefold()
-contarpalabras=texto.split()
+contarpalabras=texto.split(" ")
+for i in range(len(contarpalabras)):
+    for j in range(len(contarpalabras)):
+        if contarpalabras[i]== 
