68c67fb0a5edd5b2127d754b0180f97c4d82e96b
adriarodrisan <1900adria@gmail.com>
2024-01-09 14:04:51 +0100

    correcion del albert

--- preguntasirespuestas.py
+++ preguntasirespuestas.py
@@ -1,18 +1,22 @@
+llista_preguntes=["¿En qué año se llevó a cabo la Revolución Rusa?","¿En año se produjo la primera explosion nuclear","¿qual es el numero perfecto para Sheldon Cooper","¿Cuántos continentes hay en el mundo?","¿Cuántos elementos químicos hay en la tabla periódica?"]
+llista_respuestas=[1923,1945,73,5,118]
 seed=int(input("introduce un numero: "))
-llista_preguntes= [1,2,3,4,5,6]
-for i in range(3):
+parar=0
+puntos=0
+while parar is 0:
     seed = (seed * 997) % 1000
     random = (seed * 503) % 1000 / 100
     numero_pregunta = int(random * (len(llista_preguntes)))
-'''llista_preguntes1=["¿En qué año se llevó a cabo la Revolución Rusa?","¿En año se produjo la primera explosion nuclear","¿qual es el numero perfecto para Sheldon Cooper","¿Cuántos continentes hay en el mundo?","¿Cuántos elementos químicos hay en la tabla periódica?"]
-llista_respuestas=[1923,1945,73,5,118]
+    respuesta= input(llista_preguntes[numero_pregunta])
+    puntos +=1 if respuesta== llista_respuestas[numero_pregunta]else 0
+    parar= int(input("Si no quieres seguir jugando?[0 para seguir,1 para parar]"))
+print("Tus puntos han sido:", puntos)
+'''
 nota=0
 Com fer un número aleatòri: 
 cearem una variable anomenada seed i farem que tingui el valor
 que vulguis, llavors en el punt on vulguis fer servir un
-número aleatòri farem el següent codi.
-
-#print(llista_preguntes[numero_pregunta])
+número aleatòri farem el següent codi.  
 print(seed)
 respuesta =int(input("Respuesta"))
 if llista_respuestas[i] == respuesta:
