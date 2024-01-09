a1c405569c7d8f498cb03306314d303bb6ec9da6
adriarodrisan <1900adria@gmail.com>
2024-01-09 14:20:30 +0100

    correcion profe

--- preguntasirespuestas.py
+++ preguntasirespuestas.py
@@ -1,11 +1,11 @@
-llista_preguntes=["¿En qué año se llevó a cabo la Revolución Rusa?","¿En año se produjo la primera explosion nuclear","¿qual es el numero perfecto para Sheldon Cooper","¿Cuántos continentes hay en el mundo?","¿Cuántos elementos químicos hay en la tabla periódica?"]
-llista_respuestas=[1923,1945,73,5,118]
-seed=int(input("introduce un numero: "))
+llista_preguntes=["2+2 ","2*2 ","0/1 ","4*3 "]
+llista_respuestas=["4","4","0","12"]
+seed=int(input("INtroduce un numero mayor que 0: "))
 parar=0
 puntos=0
-while parar is 0:
+while parar == 0:
     seed = (seed * 997) % 1000
-    random = (seed * 503) % 1000 / 100
+    random = (seed * 503) % 1000 / 1000
     numero_pregunta = int(random * (len(llista_preguntes)))
     respuesta= input(llista_preguntes[numero_pregunta])
     puntos +=1 if respuesta== llista_respuestas[numero_pregunta]else 0
@@ -23,4 +23,6 @@ if llista_respuestas[i] == respuesta:
     print("Correcte!")
     nota= nota+10
 else:
-    print("Incorrecte")'''
+    print("Incorrecte")
+llista_preguntes=["¿En qué año se llevó a cabo la Revolución Rusa?","¿En año se produjo la primera explosion nuclear","¿qual es el numero perfecto para Sheldon Cooper","¿Cuántos continentes hay en el mundo?","¿Cuántos elementos químicos hay en la tabla periódica?"]
+llista_respuestas=[1923,1945,73,5,118]'''
